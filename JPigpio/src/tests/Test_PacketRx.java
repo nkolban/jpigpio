e7ac5538e9fcf4a8739e507c6ddf3e846811c42d
Jozef Sovcik <jozef@sovcik.net>
2016-04-27 11:23:54 +0200

    tests added

---
+++ JPigpio/src/tests/Test_PacketRx.java
@@ -0,0 +1,55 @@
+package tests;
+
+import jpigpio.JPigpio;
+import jpigpio.PigpioException;
+import jpigpio.PigpioSocket;
+import jpigpio.packet.*;
+
+import java.util.Arrays;
+
+
+/**
+ * Created by Jozef on 24.04.2016.
+ */
+public class Test_PacketRx {
+
+    public static void main(String[] args) throws PigpioException, InterruptedException {
+
+        int RX = 24;    // GPIO for receiving
+
+        // pigpiod host & port
+        String host = "pigpiod-host";
+        int port = 8888;
+
+        int waitForData = 20000; // milliseconds to wait for packets
+        int waitStep = 2000;
+
+        // protocol defines message length, repetition of messages, signal levels etc.
+        Protocol protocol = new Protocol();
+
+        // connect to gpigpiod instance
+        JPigpio pi = new PigpioSocket(host,port);
+        pi.gpioInitialize();
+
+        Rx rx = new Rx(pi, RX, protocol);
+
+        System.out.println("Waiting "+ waitForData+ " ms for data.");
+
+        int w = waitForData;
+        while (w > 0){
+            while (rx.available() > 0)
+                System.out.println("Received "+Arrays.toString(rx.get()));
+            Thread.sleep(waitStep);
+            w -= waitStep;
+            System.out.println("Waiting "+ w + " ms more.");
+        }
+
+        System.out.println("Terminating receiver.");
+        rx.terminate();
+
+        System.out.println("Terminating RPi connection.");
+        pi.gpioTerminate();
+
+    }
+
+}
---
+++ JPigpio/src/tests/Test_PacketTx.java
@@ -0,0 +1,65 @@
+package tests;
+
+import jpigpio.PigpioException;
+import jpigpio.PigpioSocket;
+import jpigpio.Pulse;
+import jpigpio.packet.Protocol;
+import jpigpio.packet.Tx;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Jozef on 26.04.2016.
+ */
+public class Test_PacketTx {
+
+    // GPIO pin for transmitting
+    static int TX = 26;
+
+    // pigpiod host & port
+    static String host = "pigpiod-host";
+    static int port = 8888;
+
+    // protocol defines message length, repetition of messages, signal levels etc.
+    static Protocol protocol = new Protocol();
+
+    static PigpioSocket pi;
+    static Tx tx;
+
+    public static void main(String[] args) throws PigpioException, InterruptedException {
+
+        // test message
+        byte[] TX_TEST_MSG = new byte[] {0,0,0,1,15,1,5,10,12,2};
+        byte[] TX_TEST_MSG2 = new byte[] {1,0,0,1,0,0,0,0,0,2};
+
+        int waitStep = 2000;
+        int w = 0;
+
+        // gpigpiod instance
+        pi = new PigpioSocket(host,port);
+        pi.gpioInitialize();
+
+        tx = new Tx(pi, TX, protocol);
+
+        System.out.println("Transmit test message sending "+ Arrays.toString(TX_TEST_MSG) + " " + protocol.TX_REPEAT + " times");
+        tx.put(TX_TEST_MSG);
+
+        System.out.println("Transmit test message sending "+ Arrays.toString(TX_TEST_MSG2) + " " + protocol.TX_REPEAT + " times");
+        tx.put(TX_TEST_MSG2);
+
+        while (!tx.ready()) {
+            System.out.println("Waiting for transmitter to finish its job. "+w+"ms");
+            Thread.sleep(waitStep);
+            w += waitStep;
+        }
+
+        System.out.println("Terminating transmitter.");
+        tx.terminate();
+
+        System.out.println("Terminating RPi connection.");
+        pi.gpioTerminate();
+
+    }
+
+}
