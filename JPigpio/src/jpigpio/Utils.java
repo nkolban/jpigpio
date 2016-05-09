68d2d4e2b807409f6e5d3d8e9186bf0be164e91e
Jozef Sovcik <jozef@sovcik.net>
2016-05-09 20:55:17 +0200

    removing debug printouts

--- JPigpio/src/jpigpio/Utils.java
+++ JPigpio/src/jpigpio/Utils.java
@@ -17,9 +17,9 @@ public class Utils {
 	
 	public static int mapToInt(int value, int sourceLow, int sourceHigh, int targetLow, int targetHigh) {
 		double pos = (value-sourceLow)/(double)(sourceHigh - sourceLow);
-		System.out.println("Pos: " + pos);
-		System.out.println("tHigh - tLow = " + (targetHigh - targetLow));
-		System.out.println("r = " + (targetLow + ((targetHigh - targetLow) *  pos)));
+		//System.out.println("Pos: " + pos);
+		//System.out.println("tHigh - tLow = " + (targetHigh - targetLow));
+		//System.out.println("r = " + (targetLow + ((targetHigh - targetLow) *  pos)));
 		return (int)(targetLow + ((targetHigh - targetLow) *  pos));
 	}
 	
